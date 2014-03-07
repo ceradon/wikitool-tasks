2f250c9f690718e39d34291c9996bcedda281daf
ceradon <ceradon_wiki@hotmail.com>
2014-03-06 17:00:24 -0800

    Update didyouknow.py

--- didyouknow.py
+++ didyouknow.py
@@ -9,54 +9,46 @@ class DYKReport(BorgInit):
     """
     Wikipedia bot that notifies editors that their atricles
     have been nominated to be on the Did You Know section of
-    the Main Page."""
+    the Main Page.
+    """
 
     def __init__(self):
-        self._site = Wiki() # Initiate Wiki instance.
+        self._site = Wiki()
         super(DYKReport, self).__init__()
-        
-        user = "Cerabot"            # Retrieve vital login data
-        passw = self._pass_retrieve  # for bot account.
-        
-        self._site.login(user, passw) # Now login.
-        
-        # "Created by" and "Nominated by" regular expressions
-        """
-        self._nominator_is_not_creator_regex = compile(
-            r"Created by \[\[User:(.*?)\|.*?\]\] \(\[\[(.*?)\|" +
-            r"talk\]\]\). Nominated by \[\[User:(.*?)\|.*?\]\]" +
-            r" \(\[\[(.*?)\|talk\]\]\) at (.*?)\<")
 
-        self._creator_is_nominator_regex = compile(
-            r"(\<small\>)?Created by (\[\[User:(.*?)\|.*?\]\]) " +
-            r"\(\[\[(.*?)\|talk\]\]\)\. Self-? ?nominated at " +
-            r"(.*?)\</small>.")
-        """
+        user = "Cerabot"
+        passw = self._pass_retrieve
 
-        del user; del passw # Delete the login data.
+        self._site.login(user, passw)
 
-    def _parse_page(self, page="Template talk:Did you know"):
-        dyk = Page(self._site, title=page) # Initiate Page instance
-        text = dyk.getWikiText()           # and get text of page.
+        del user; del passw
 
-        # Parse the page's content for wikicode and gather all the
-        # templates that begin with "{{Did you know"
+    def _parse_page(self, page="Template talk:Did you know", 
+            handle_old_pages=False):
+        dyk = Page(self._site, title=page)
+        text = dyk.getWikiText()
+
         text = text.decode("utf-8")
         parsed = Parser.parse(text)
         templates = [a for a in parsed.filter_templates() if 
                         str(a).startswith("{{Did you know")]
 
-        # Iterate over the template list and get all page names
-        pages = {
-             "nominator_is_creator": [],
-             "nominator_is_not_creator": [],
-             }
+        dyk_nom_not_creator = []
         for template in templates:
+            creator_is_nominator = False
             name = str(template.name)
             page = Page(self._site, title=name)
-            wikitext = page.getWikiText()
-            x = findall(r"\<small\>(.*?)\</small\>")
-            a = [b for b in x if "by [[" in b]
+            page_creator = page.getHistory(direction="newer",
+                content=False, limit=1)[1]["user"]
+            dyk_creator = page.getHistory(direction="newer",
+                content=False, limit=1)[1]["user"]
+            if page_creator.lower() != dyk_creator.lower():
+                creator_is_nominator = True
+                dyk_nom_not_creator.append((name.split("/")[1],
+                    ))
+            else:
+                continue
+        return dyk_nom_is_creator
 
 if __name__ == "__main__":
     print "Troll."

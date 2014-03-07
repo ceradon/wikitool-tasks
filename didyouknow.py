340584434455d2aaf878f4862418d8978397a980
ceradon <ceradon_wiki@hotmail.com>
2014-03-06 20:03:00 -0800

    Make a little more efficient.

--- didyouknow.py
+++ didyouknow.py
@@ -31,41 +31,48 @@ class DYKReport(BorgInit):
         parsed = Parser.parse(text)
         templates = [a for a in parsed.filter_templates() if 
                         str(a).startswith("{{Did you know")]
-
-        dyk_nom_not_creator = []
+        newtemplates = []
         for template in templates:
-            creator_is_nominator = False
             name = str(template).replace("{{", "Template:").replace(
                 "}}", "")
             page = Page(self._site, title=name)
+            wikitext = page.getWikiText()
             page_creator = page.getHistory(direction="newer",
                 content=False, limit=1)[0]["user"]
             dyk_creator = page.getHistory(direction="newer",
                 content=False, limit=1)[0]["user"]
             if page_creator.lower() != dyk_creator.lower():
+                newtemplates.append((page, page_creator, dyk_creator))
+            else:
+                continue
+
+        dyk_nom_not_creator = []
+        for template in newtemplates:
+            creator_is_nominator = False
+            if template[1].lower() != template[2].lower():
                 creator_is_nominator = True
                 matched = findall(r"\<small\>(.*?)\</small\>")
                 matched = match.group(1)
                 x = compile(r"\[\[User:(.*?)\|.*?\]\] \(\[\[.*?\|" +
                     r"talk\]\]\)")
                 creators = x.match(matched.split("&nbsp;")[0])
-                if len(creators.remove(page_creator)) > 0:
+                if len(creators.remove(template[1])) > 0:
                     dyk_nom_not_creator.append({
                         "pagename":name.split("/")[1],
-                        "pagecreator":creators.append(page_creator),
+                        "pagecreator":creators.append(template[1]),
                         "dykname":name,
-                        "dykcreator":dyk_creator,
+                        "dykcreator":template[2],
                         })
                 else:
                     dyk_nom_not_creator.append({
                         "pagename":name.split("/")[1],
-                        "pagecreator":[page_creator],
+                        "pagecreator":[template[1]],
                         "dykname":name,
-                        "dykcreator":dyk_creator,
+                        "dykcreator":template[2],
                         })
             else:
                 continue
-        return dyk_nom_is_creator
+        return dyk_nom_not_creator
 
 if __name__ == "__main__":
     print "Troll."

1e946c54e420ec57fd101bc8bc5bbb58201f5d02
ceradon <ceradon_wiki@hotmail.com>
2014-03-23 19:23:53 -0700

    Fix indentation.

--- didyouknow.py
+++ didyouknow.py
@@ -89,7 +89,7 @@ class DYKReport(BorgInit):
                     d = dyk.getHistory(direction="newer", content=False, 
                         limit=1)[0]
                 except Exception, e:
-                     error = "The page prolly doesn't exist, but I'm " \
+                    error = "The page prolly doesn't exist, but I'm " \
                         "speculating. Here's the error thrown: {0}. "
                     print error.format(e) + "I'll go on."
                     continue

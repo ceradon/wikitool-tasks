9af4aac3047a2e0323679c77b18cfd1595ad2702
ceradon <ceradon_wiki@hotmail.com>
2014-03-31 17:55:03 -0700

    Update didyouknow.py

--- didyouknow.py
+++ didyouknow.py
@@ -134,7 +134,7 @@ class DYKReport(BorgInit):
             message = "\n==Message from Cerabot==\n" \
                     "{{SUBST:User:Cerabot/Umbox|article=%s|" \
                     "include_links=yes}}" % title.split("/")[1]
-            newtext = text + message
+            newtext = text + "{" + message + "}"
             summary = "Notifying [[User:{0}|{0}]] of [[{1}|Did you " \
                 "know nomination]] ([[User:Cerabot/Run/Task 2|" \
                 "bot task]])".format(user.name, user_talk.title)
